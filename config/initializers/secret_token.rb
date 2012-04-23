0ac1405cde2a8f5f5e1deaec455c7a7c8afff164
Andrea D'Amico <andrea.damico@gmail.com>
2012-04-23 16:10:59 +0200

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Immunotoxibase::Application.config.secret_token = 'e467ec61427911cf5d23ceb1bdfce5de12d6d08a7c65d5e71127a145bf7fa77ed53d9b1d87aa85302eb0c39f1781d4e33837a30bae662e760b538d3491e70487'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Immunotoxibase::Application.routes.draw do
+  root :to => "home#index"
+end
