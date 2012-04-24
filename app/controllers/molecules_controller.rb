c5baca8dd35cb399d07fd418ffb7d00c0e36f88f
Andrea D'Amico <andrea.damico@gmail.com>
2012-04-24 16:57:12 +0200

    minor tweaks css, new action

--- app/controllers/molecules_controller.rb
+++ app/controllers/molecules_controller.rb
@@ -1,8 +1,11 @@
 class MoleculesController < ApplicationController
   load_and_authorize_resource
+
   def index
     @molecules = @molecules.text_search(params[:query]).page(params[:page]).per(10)
   end
-  def show
-  end
+
+  def show ; end
+  
+  def new ; end
 end
