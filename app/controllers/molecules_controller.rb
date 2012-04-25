a30fa08acffe33f48406a78fe0d39aa4d443718f
Andrea D'Amico <andrea.damico@gmail.com>
2012-04-25 15:08:34 +0200

    crud actions for molecules controllersé

--- app/controllers/molecules_controller.rb
+++ app/controllers/molecules_controller.rb
@@ -5,7 +5,24 @@ class MoleculesController < ApplicationController
     @molecules = @molecules.text_search(params[:query]).page(params[:page]).per(10)
   end
 
-  def show ; end
-  
-  def new ; end
+  def create
+    if @molecule.save
+      redirect_to @molecule, notice: "Successfully created #{@molecule}"
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @molecule.update_attributes(params[:molecule])
+      redirect_to @molecule, notice: "Successfully updated #{@molecule}"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @molecule.destroy
+    redirect_to molecules_path, notice: "Successfully destroyed #{@molecule}"
+  end
 end
