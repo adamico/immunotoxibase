17b0ffc21f7e9116eb8b24438a8e25e502cdebdd
Andrea D'Amico <andrea.damico@gmail.com>
2012-04-25 15:09:05 +0200

    edit action, tweaking buttons, refactoring link_to_if helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,26 @@
 module ApplicationHelper
-  def link_if_can(text, action, resource, options={})
+  def link_if_can(text, action, resource, icon="", options={})
     text ||= "#{action.to_s.capitalize} #{resource}"
-    path = case action
-           when :new
-             new_polymorphic_path(resource)
-           when :edit
-             edit_polymorphic_path(resource)
-           else
-             polymorphic_path(action, resource)
-           end
-    link_to(text, path, options) if can?(action, resource)
+    case action
+    when :new
+      path = new_polymorphic_path(resource)
+      icon = "plus"
+      css = "success"
+    when :edit
+      path = edit_polymorphic_path(resource)
+      icon = "pencil"
+      css = "warning"
+    when :destroy
+      path = polymorphic_path(resource)
+      icon = "trash"
+      css = "danger"
+      options = {confirm: "Are you sure?", method: :delete}
+    end
+    options = options.merge(class: "btn btn-#{css}")
+    if can?(action, resource)
+      link_to(path, options) do
+        content_tag(:i, "", class: "icon-#{icon} icon-white") + " #{text}"
+      end
+    end
   end
 end
