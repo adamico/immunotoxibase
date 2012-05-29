c78201e51d8b9608e006f6e8d818d3ea2b5cc461
Andrea D'Amico <andrea.damico@gmail.com>
2012-05-29 15:44:37 +0200

    breadcrumb changed

--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -1,12 +1,19 @@
 module SectionsHelper
-  def breadcrumb(item)
-    content_tag :ul, class: "breadcrumb" do
-      lis = []
-      lis << toc_crumb
-      item.self_and_ancestors.each do |branch|
-        lis << crumb(branch)
+  def page_title(section)
+    title = section ? [section.name, section.link_to_pdf].compact.join("").html_safe : "Table of Contents"
+    content_tag :h1, title
+  end
+
+  def breadcrumb(section)
+    if section
+      content_tag :ul, class: "breadcrumb" do
+        lis = []
+        lis << toc_crumb
+        section.self_and_ancestors.each do |branch|
+          lis << crumb(branch)
+        end
+        lis.join("").html_safe
       end
-      lis.join("").html_safe
     end
   end
 
@@ -21,10 +28,10 @@ module SectionsHelper
   end
 
   def crumb(item)
-    not_current = item.id != params[:id].to_i
+    not_current = item.id != params[:section].to_i
     item_class = not_current ? nil : "active"
     content_tag(:li, nil, class: item_class) do
-      content = link_to_if(not_current, item.name, item)
+      content = link_to_if(not_current, item.name, toc_path(section: item))
       content += crumb_divider if not_current
       content.html_safe
     end.html_safe
--- spec/helpers/sections_helper_spec.rb
+++ spec/helpers/sections_helper_spec.rb
@@ -5,24 +5,29 @@ describe SectionsHelper do
     let(:chapter) {Section.create!(name: "chapter")}
     let(:family) {Section.create!(name: "family", parent_id: chapter.id)}
     let(:molecule) {Section.create!(name: "molecule", parent_id: family.id)}
+    context "when provided item is nil" do
+      it "displays nothing" do
+        helper.breadcrumb(nil).should == nil
+      end
+    end
     context "when current item is a root element" do
       it "displays only a link to the table of contents" do
-        controller.params = {controller: "sections", action: "show", id: chapter.id}
+        controller.params = {controller: "sections", action: "toc", section: chapter.id}
         html = '<ul class="breadcrumb"><li><a href="/toc">Table of Contents</a> <span class="divider">&gt;</span></li><li class="active">chapter</li></ul>'
         helper.breadcrumb(chapter).should == html
       end
     end
     context "when item has 1 ancestor" do
       it "displays a breadcrumb with item name preceded by ancestor name + ' > '" do
-        controller.params = {controller: "sections", action: "show", id: family.id}
-        html = "<ul class=\"breadcrumb\"><li><a href=\"/toc\">Table of Contents</a> <span class=\"divider\">&gt;</span></li><li><a href=\"/sections/#{chapter.id}\">chapter</a> <span class=\"divider\">&gt;</span></li><li class=\"active\">family</li></ul>"
+        controller.params = {controller: "sections", action: "toc", section: family.id}
+        html = "<ul class=\"breadcrumb\"><li><a href=\"/toc\">Table of Contents</a> <span class=\"divider\">&gt;</span></li><li><a href=\"/toc?section=#{chapter.id}\">chapter</a> <span class=\"divider\">&gt;</span></li><li class=\"active\">family</li></ul>"
         helper.breadcrumb(family).should == html
       end
     end
     context "when item has 2 ancestors" do
       it "displays a breadcrumb with item name preceded by ancestor name + ' > ' for each ancestor" do
-        controller.params = {controller: "sections", action: "show", id: molecule.id}
-        html = "<ul class=\"breadcrumb\"><li><a href=\"/toc\">Table of Contents</a> <span class=\"divider\">&gt;</span></li><li><a href=\"/sections/#{chapter.id}\">chapter</a> <span class=\"divider\">&gt;</span></li><li><a href=\"/sections/#{family.id}\">family</a> <span class=\"divider\">&gt;</span></li><li class=\"active\">molecule</li></ul>"
+        controller.params = {controller: "sections", action: "toc", section: molecule.id}
+        html = "<ul class=\"breadcrumb\"><li><a href=\"/toc\">Table of Contents</a> <span class=\"divider\">&gt;</span></li><li><a href=\"/toc?section=#{chapter.id}\">chapter</a> <span class=\"divider\">&gt;</span></li><li><a href=\"/toc?section=#{family.id}\">family</a> <span class=\"divider\">&gt;</span></li><li class=\"active\">molecule</li></ul>"
         helper.breadcrumb(molecule).should == html
       end
     end
