671617e969700e6feb1030fa6de5abc423c9cbf3
Andrea D'Amico <andrea.damico@gmail.com>
2012-06-27 14:48:54 +0200

    fixing sections helper

--- spec/helpers/sections_helper_spec.rb
+++ spec/helpers/sections_helper_spec.rb
@@ -12,19 +12,19 @@ describe SectionsHelper do
     end
     context "when current item is a root element" do
       it "displays link to TOC" do
-        html = "<ul class=\"breadcrumb\"><li><a href=\"/toc\">Table of Contents</a></li><li class=\"active\"><span class=\"divider\">&gt;</span><i class=\"icon-book\"></i> chapter</li></ul>"
+        html = "<div class=\"subnav\"><ul class=\"breadcrumb\"><li><a href=\"/toc\">Table of Contents</a></li><li class=\"active\"><span class=\"divider\">&gt;</span><i class=\"icon-white icon-book\"></i> chapter</li></ul></div>"
         helper.breadcrumb(chapter, "chapter").should == html
       end
     end
     context "when item has 1 ancestor" do
       it "displays TOC > Ancestor > item" do
-        html = "<ul class=\"breadcrumb\"><li><a href=\"/toc\">Table of Contents</a></li><li><span class=\"divider\">&gt;</span><a href=\"/toc/chapter\"><i class=\"icon-book\"></i> chapter</a></li><li class=\"active\"><span class=\"divider\">&gt;</span><i class=\"icon-file\"></i> family</li></ul>"
+        html = "<div class=\"subnav\"><ul class=\"breadcrumb\"><li><a href=\"/toc\">Table of Contents</a></li><li><span class=\"divider\">&gt;</span><a href=\"/toc/chapter\"><i class=\"icon-white icon-book\"></i> chapter</a></li><li class=\"active\"><span class=\"divider\">&gt;</span><i class=\"icon-white icon-file\"></i> family</li></ul></div>"
         helper.breadcrumb(family, "family").should == html
       end
     end
     context "when item has 2 ancestors" do
       it "displays a breadcrumb with item name preceded by ancestor name + ' > ' for each ancestor" do
-        html = "<ul class=\"breadcrumb\"><li><a href=\"/toc\">Table of Contents</a></li><li><span class=\"divider\">&gt;</span><a href=\"/toc/chapter\"><i class=\"icon-book\"></i> chapter</a></li><li><span class=\"divider\">&gt;</span><a href=\"/toc/chapter/family\"><i class=\"icon-file\"></i> family</a></li><li class=\"active\"><span class=\"divider\">&gt;</span><i class=\"icon-tint\"></i> molecule</li></ul>"
+        html = "<div class=\"subnav\"><ul class=\"breadcrumb\"><li><a href=\"/toc\">Table of Contents</a></li><li><span class=\"divider\">&gt;</span><a href=\"/toc/chapter\"><i class=\"icon-white icon-book\"></i> chapter</a></li><li><span class=\"divider\">&gt;</span><a href=\"/toc/chapter/family\"><i class=\"icon-white icon-file\"></i> family</a></li><li class=\"active\"><span class=\"divider\">&gt;</span><i class=\"icon-white icon-tint\"></i> molecule</li></ul></div>"
         helper.breadcrumb(molecule, "molecule").should == html
       end
     end
