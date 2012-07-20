2a6ad434ab41e8e47f315204015a0acc1e9ef20e
Andrea D'Amico <andrea.damico@gmail.com>
2012-07-20 14:51:40 +0200

    using aws-sdk with paperclip

--- Gemfile
+++ Gemfile
@@ -44,7 +44,7 @@ gem 'coffee-filter'
 gem 'awesome_nested_set', '~> 2.1.0'
 gem 'the_sortable_tree', "~> 1.9.2"
 gem "paperclip", :git => "git://github.com/thoughtbot/paperclip.git"
-gem "fog"
+gem "aws-sdk"
 gem "draper"
 gem "friendly_id", "~> 4.0.1"
 gem 'acts_as_list'
--- app/models/section.rb
+++ app/models/section.rb
@@ -7,11 +7,11 @@ class Section < ActiveRecord::Base
 
   acts_as_nested_set
 
-  has_attached_file :picture, fog_credentials: {
-                      aws_access_key_id: ENV['S3_KEY'],
-                      aws_secret_access_key: ENV['S3_SECRET'],
-                      provider: 'AWS'
-                    }
+  has_attached_file :picture, bucket: ENV['S3_BUCKET'],
+                      s3_credentials: {
+                        access_key_id: ENV['S3_KEY'],
+                        secret_access_key: ENV['S3_SECRET']
+                      }
 
   validates :name, presence: true
 
