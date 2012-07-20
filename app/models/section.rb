644a7f34c51a134142bf17e8fb76588cbdb7867b
Andrea D'Amico <andrea.damico@gmail.com>
2012-07-20 16:47:32 +0200

    adding storage s3 option for paperclip

--- app/models/section.rb
+++ app/models/section.rb
@@ -7,7 +7,9 @@ class Section < ActiveRecord::Base
 
   acts_as_nested_set
 
-  has_attached_file :picture, bucket: ENV['S3_BUCKET'],
+  has_attached_file :picture,
+                      storage: :s3,
+                      bucket: ENV['S3_BUCKET'],
                       s3_credentials: {
                         access_key_id: ENV['S3_KEY'],
                         secret_access_key: ENV['S3_SECRET']
